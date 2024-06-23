068768ba301524879ad89679abe91d68cd8f612e
maekawa_shintaro <maeshinshin@outlook.jp>
2024-06-24 03:05:05 +0900

    :books: add files

---
+++ abc211/c/main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+const (
+	chokudai = "chokudai"
+	num      = 8
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, 64*1024), 1e6)
+	s := scanString()
+	dp := make([][]int64, len(s)+1)
+	for i := range dp {
+		dp[i] = make([]int64, num+1)
+		dp[i][0] = 1
+	}
+	for i := range dp {
+		if i == 0 {
+			continue
+		}
+		for j := range dp[i] {
+			if j == 0 {
+				continue
+			}
+			if s[i-1] == chokudai[j-1] {
+				dp[i][j] = dp[i-1][j] + dp[i-1][j-1]
+			} else {
+				dp[i][j] = dp[i-1][j]
+			}
+			if dp[i][j] >= 1e9+7 {
+				dp[i][j] -= 1e9 + 7
+			}
+		}
+	}
+	fmt.Println(dp[len(s)][num])
+}
+
+func max(a, b int64) int64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func abs(a int64) int64 {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+// slice
+func sum(a []int64) (s int64) {
+	for _, v := range a {
+		s += v
+	}
+	return
+}
+
+// algorithm
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func nextPermutation(x sort.Interface) bool {
+	n := x.Len()
+	if n <= 1 {
+		return false
+	}
+
+	i := n - 2
+	for i >= 0 && !x.Less(i, i+1) {
+		i--
+	}
+	if i < 0 {
+		reverse(x, 0, n-1)
+		return false
+	}
+
+	j := n - 1
+	for !x.Less(i, j) {
+		j--
+	}
+
+	x.Swap(i, j)
+	reverse(x, i+1, n-1)
+	return true
+}
+
+func reverse(x sort.Interface, start, end int) {
+	for start < end {
+		x.Swap(start, end)
+		start++
+		end--
+	}
+}
+
+// heap queue
+type IntHeap []int64
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int64))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+// scan
+func scanInt64() (i int64) {
+	var err error
+	sc.Scan()
+	i, err = strconv.ParseInt(sc.Text(), 10, 64)
+	if err != nil {
+		log.Fatal("Failed to input", err)
+	}
+	return
+}
+
+func scanString() (s string) {
+	sc.Scan()
+	s = sc.Text()
+	return
+}
